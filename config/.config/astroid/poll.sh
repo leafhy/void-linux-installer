6244726a270defcbea824d192fab28ad15089ed9
leafhy <lanf89y@gmail.com>
2020-12-16 02:21:03 +1030

    Create poll.sh

---
+++ config/.config/astroid/poll.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+#
+# poll.sh - An example poll script for astroid.
+#
+# Intended to be run by astroid. See the following link for more information:
+# https://github.com/astroidmail/astroid/wiki/Polling
+#
+# In particular, in order for this script to be run by astroid, it needs to be
+# located at ~/.config/astroid/poll.sh
+#
+
+# Exit as soon as one of the commands fail.
+set -e
+
+# Fetch new mail.
+# offlineimap
+
+# Import new mail into the notmuch database.
+notmuch new
+
+# Here you can process the mail in any way you see fit. See the following link
+# for examples:
+# https://github.com/astroidmail/astroid/wiki/Advanced-Polling-and-Processing-mail#automatic-tagging
