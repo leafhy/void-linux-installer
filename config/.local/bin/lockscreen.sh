4d0f9b3c68a0771cb88c28ffab1c506712fc743c
leafhy <lanf89y@gmail.com>
2021-08-20 22:24:43 +0930

    Update lockscreen.sh

--- config/.local/bin/lockscreen.sh
+++ config/.local/bin/lockscreen.sh
@@ -8,9 +8,9 @@ set -e
 screen=$(xrandr --listactivemonitors | grep -- "-1" | cut -d " " -f 6)
 
 if [[ $screen = "DP-1" ]] || [[ $screen = "LVDS-1" ]] || [[ $screen = "VGA-1" ]]; then
-amixer set Master mute && sakura -s -x asciiquarium & alock -bg none; xdotool key --clearmodifiers q
+amixer set Master mute >/dev/null && sakura -s -x asciiquarium 2>/dev/null & alock -bg none; xdotool key --clearmodifiers q
 
 else 
-amixer set Master mute && ~/.config/i3/lock.sh
+amixer set Master mute >/dev/null && ~/.config/i3/lock.sh
 
 fi
