081aa108d02d285e4988a842596e9eb0082b79b8
leafhy <lanf89y@gmail.com>
2021-08-20 23:00:12 +0930

    Create rofi-confirm.sh

---
+++ config/.config/rofi/scripts/rofi-confirm.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+ICON="$1"
+
+CANCEL="No"
+CONFIRM="Yes"
+
+case "$(echo -e "$CANCEL\n$CONFIRM" | rofi  -dmenu -theme ~/.config/rofi/options-menu.rasi -mesg "$ICON" )" in
+    "$CANCEL")  exit 1;;
+    "$CONFIRM") exit 0;;
+esac
+
+exit 1
