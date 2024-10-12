f0f08bbffe49299a8669c1459d023c62ea89e3ac
leafhy <7463804+leafhy@users.noreply.github.com>
2024-10-12 15:48:24 +1030

    update

--- config/.local/bin/lockscreen.sh
+++ config/.local/bin/lockscreen.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 ##################### WARNING ######################
-# i3 lock allows notifications
-# Underlying text can show on asciiquarium
+# i3 lock allows notifications.
+# Underlying text can show on asciiquarium.
 ####################################################
 set -e
 
 # Mute ALL sound cards
-grep ' \[' /proc/asound/cards | cut -d ' ' -f 2 | \
+awk '/ \[/ { print $1 }' /proc/asound/cards | \
    while read card
    do
    amixer --quiet -c "$card" sset "$(amixer -c "$card" | head -n 1 | awk -F\' '{ print $2 }')" mute
-done
+   done
 
-screen=$(xrandr --listactivemonitors | grep -- "-1" | cut -d " " -f 6)
+{
+read screen1
+read screen2
+} < <(xrandr --listactivemonitors | awk '/-1/ { print $4 }')
+
+screen=$screen1$screen2
 
 if [[ $screen = "DP-1" ]] || [[ $screen = "LVDS-1" ]] || [[ $screen = "VGA-1" ]]; then
     sakura -s -x asciiquarium 2>/dev/null & alock -bg none; xdotool key --clearmodifiers q
-else
+
+elif [[ $screen1 ]] && [[ $screen2 ]]; then
     ~/.config/i3/new-lock.sh
 fi
